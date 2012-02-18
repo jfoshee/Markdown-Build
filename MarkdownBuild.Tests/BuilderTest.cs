e8cbcf17e83479b2b5087b0a792c9ebd68a1e97c
jfoshee <jacobf@gmail.com>
2012-02-18 16:21:55 -0600

    Added projects, references, and Builder.Transform(markdownFile, htmlFile)

---
+++ MarkdownBuild.Tests/BuilderTest.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using MarkdownSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestDrivenDesign;
+
+namespace MarkdownBuild.Tests
+{
+    [TestClass]
+    public class BuilderTest : TestBase<Builder>
+    {
+        [TestMethod]
+        public void ShouldHaveMarkdownInstance()
+        {
+            Markdown markdown = Subject.Markdown;
+            Assert.IsNotNull(markdown);
+        }
+
+        [TestMethod]
+        public void ShouldTransformFileToHtml()
+        {
+            // Arrange
+            var directory = TestDirectory();
+            var markdownFile = Path.Combine(directory, "test.md");
+            var htmlFile = Path.Combine(directory, "test.html");
+            File.WriteAllText(markdownFile, "## Test");
+
+            // Act
+            Subject.TransformFile(markdownFile, htmlFile);
+
+            // Assert
+            TextFileAssert.Contains(htmlFile, "<h2>Test");
+        }
+    }
+}
---
+++ MarkdownBuild/Builder.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using MarkdownSharp;
+
+namespace MarkdownBuild
+{
+    public class Builder
+    {
+        public Markdown Markdown { get; set; }
+
+        public Builder()
+        {
+            Markdown = new Markdown();
+        }
+
+        public void TransformFile(string markdownFile, string htmlFile)
+        {
+            var md = File.ReadAllText(markdownFile);
+            var html = Markdown.Transform(md);
+            File.WriteAllText(htmlFile, html);
+        }
+    }
+}
