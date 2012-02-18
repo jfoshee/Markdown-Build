ec42202f1788a30112d1e7912fbb8b3322d7ac05
jfoshee <jacobf@gmail.com>
2012-02-18 17:10:11 -0600

    Transforming a file with a header and footer and transforming all files in a directory (non-exclusive and non-recursive)

--- MarkdownBuild.Tests/BuilderTest.cs
+++ MarkdownBuild.Tests/BuilderTest.cs
@@ -21,8 +21,8 @@ namespace MarkdownBuild.Tests
             // Arrange
             var directory = TestDirectory();
             var markdownFile = Path.Combine(directory, "test.md");
-            var htmlFile = Path.Combine(directory, "test.html");
             File.WriteAllText(markdownFile, "## Test");
+            var htmlFile = Path.Combine(directory, "test.html");
 
             // Act
             Subject.TransformFile(markdownFile, htmlFile);
@@ -30,5 +30,67 @@ namespace MarkdownBuild.Tests
             // Assert
             TextFileAssert.Contains(htmlFile, "<h2>Test");
         }
+
+        [TestMethod]
+        public void ShouldTransformFileWithHeaderAndFooter()
+        {
+            // Arrange
+            var directory = TestDirectory();
+            var markdownFile = Path.Combine(directory, "test.md");
+            File.WriteAllText(markdownFile, "### Content");
+            var htmlFile = Path.Combine(directory, "test.html");
+            var header = "~~ Header! ~~";
+            var footer = "@@ Footer! @@";
+
+            // Act
+            Subject.TransformFile(markdownFile, htmlFile, header, footer);
+
+            // Assert
+            TextFileAssert.StartsWith(htmlFile, header);
+            TextFileAssert.Contains(htmlFile, "<h3>Content");
+            TextFileAssert.EndsWith(htmlFile, footer);
+        }
+
+        [TestMethod]
+        public void ShouldTransformAllFilesInDirectory()
+        {
+            // Arrange
+            var destinationDirectory = Path.Combine(TestDirectory(), "Dest");
+            var sourceDirectory = Path.Combine(TestDirectory(), "Source");
+            Directory.CreateDirectory(sourceDirectory);
+            File.WriteAllText(Path.Combine(sourceDirectory, "a.md"), "_A_");
+            File.WriteAllText(Path.Combine(sourceDirectory, "b.md"), "_B_");
+
+            // Act
+            Subject.TransformFiles(sourceDirectory, destinationDirectory);
+
+            // Assert
+            DirectoryAssert.Exists(destinationDirectory);
+            TextFileAssert.Contains(Path.Combine(destinationDirectory, "a.html"), "<em>A</em>");
+            TextFileAssert.Contains(Path.Combine(destinationDirectory, "b.html"), "<em>B</em>");
+        }
+
+        // TODO: Recursive
+        // TODO: _header and _footer
+        // TODO: Just copy non markdown files
+        // TODO: Reference Style.css
+
+        //[TestMethod]
+        //public void ShouldInjectStyleReferenceIfCssFileFound()
+        //{
+        //    // Arrange
+        //    var directory = TestDirectory();
+        //    var stylePath = Path.Combine(directory, "style.css");
+        //    File.WriteAllText(stylePath, @"p{ font-family:""Times New Roman""; }");
+        //    var markdownFile = Path.Combine(directory, "foo.md");
+        //    File.WriteAllText(markdownFile, "## Test");
+        //    var htmlFile = Path.Combine(directory, "bar.html");
+
+        //    // Act
+        //    Subject.TransformFile(markdownFile, htmlFile);
+
+        //    // Assert
+        //    TextFileAssert.Contains(htmlFile, @"<link rel=""stylesheet"" type=""text/css"" href=""style.css"" />");
+        //}
     }
 }
--- MarkdownBuild/Builder.cs
+++ MarkdownBuild/Builder.cs
@@ -14,9 +14,25 @@ namespace MarkdownBuild
 
         public void TransformFile(string markdownFile, string htmlFile)
         {
+            TransformFile(markdownFile, htmlFile, "", "");
+        }
+
+        public void TransformFile(string markdownFile, string htmlFile, string header, string footer)
+        {
             var md = File.ReadAllText(markdownFile);
-            var html = Markdown.Transform(md);
+            var html = header + Markdown.Transform(md) + footer;
             File.WriteAllText(htmlFile, html);
+        }
+
+        public void TransformFiles(string sourceDirectory, string destinationDirectory)
+        {
+            Directory.CreateDirectory(destinationDirectory);
+            foreach (var md in Directory.EnumerateFiles(sourceDirectory))
+            {
+                var filename = Path.GetFileNameWithoutExtension(md);
+                var html = Path.Combine(destinationDirectory, filename + ".html");
+                TransformFile(md, html);
+            }
         }
     }
 }
