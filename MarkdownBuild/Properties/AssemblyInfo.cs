71574fb616a067f764d842fa35b6bf115482dbc3
jfoshee <jacobf@gmail.com>
2012-02-18 16:23:43 -0600

    Assembly info

--- MarkdownBuild/Properties/AssemblyInfo.cs
+++ MarkdownBuild/Properties/AssemblyInfo.cs
@@ -1,12 +1,12 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MarkdownBuild")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Builds a web site from a set of markdown files")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Unplugged Development")]
 [assembly: AssemblyProduct("MarkdownBuild")]
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: CLSCompliant(true)]
