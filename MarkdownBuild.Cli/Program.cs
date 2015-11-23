0d564c6843e055a964faac248bcdf1a8780c6d01
Jacob Foshee <jacobf@gmail.com>
2015-11-23 10:06:36 -0600

    Added basic command line

---
+++ MarkdownBuild.Cli/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MarkdownBuild.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine(@"Usage: 
+mdbuild <input dir> <output dir>");
+                return;
+            }
+            var sourceDirectory = args[0];
+            var destinationDirectory = args[1];
+            var builder = new Builder();
+            try
+            {
+                builder.TransformFiles(sourceDirectory, destinationDirectory);
+            }
+            catch (Exception exception)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Error.WriteLine("Error: {0}", exception.Message);
+            }
+            Console.ResetColor();
+        }
+    }
+}
